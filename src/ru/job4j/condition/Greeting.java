bad08a1d21c570717870aa120f4a151049dc4f1d
Stas <bazhoker@gmail.com>
2020-07-24 12:01:26 +0300

    9.1. String [#332830]

--- src/ru/job4j/condition/Greeting.java
+++ src/ru/job4j/condition/Greeting.java
@@ -4,7 +4,8 @@ public class Greeting {
     public static void main(String[] args) {
         String idea = "I like Java!";
         System.out.println(idea);
-        System.out.println(idea + "But I am a newbie");
+
+        idea += "But I am a newbie";
         int year = 2020;
         idea = idea + year;
         System.out.println(idea);
