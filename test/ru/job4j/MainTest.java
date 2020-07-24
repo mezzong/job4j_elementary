b35b7e437ffbcb4cfd4674eef3b5c8a0c5b1f054
Stas <bazhoker@gmail.com>
2020-07-24 15:29:49 +0300

    2. Тесты для идеальный вес [#332860]

--- test/ru/job4j/MainTest.java
+++ test/ru/job4j/MainTest.java
@@ -7,8 +7,8 @@ import static org.junit.Assert.assertThat;
 public class MainTest {
     @Test
     public void whenActionThenResult() {
-        int excepted = 1;
+        int expected = 1;
         int result = 1;
-        assertThat(result, is(excepted));
+        assertThat(result, is(expected));
     }
 }
