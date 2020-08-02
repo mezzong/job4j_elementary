932fbf470fe9c02006f4ffb493c35bfeac840acf
Stas <bazhoker@gmail.com>
2020-08-02 13:43:19 +0300

    6.0.2. Заполнение массива. [#332805]

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -5,5 +5,13 @@ public class ArrayDefinition {
         short[] ages = new short[10];
         String[] surnames = new String[100500];
         float[] prices = new float[40];
+        String[] names = new String[4];
+        names[0] = "Petr";
+        names[1] = "Alexey";
+        names[2] = "Ivan";
+        names[3] = "Roma";
+        for (int i = 0; i < 4; i++) {
+            System.out.println(names[i]);
+        }
     }
 }
