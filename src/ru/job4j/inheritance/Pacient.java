a1369ae0790cd785319c02c1cf7d375c10df9e0e
Stas <bazhoker@gmail.com>
2020-09-08 11:17:25 +0300

    3. Реализация профессий в коде [#332996]

---
+++ src/ru/job4j/inheritance/Pacient.java
@@ -0,0 +1,31 @@
+package ru.job4j.inheritance;
+
+public class Pacient {
+    private String firstName;
+    private String surName;
+    private int age;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getSurName() {
+        return surName;
+    }
+
+    public void setSurName(String surName) {
+        this.surName = surName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
